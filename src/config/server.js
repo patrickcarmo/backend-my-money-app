03889f2d46e830c48b52c8d4690c396ed8fdd103
Patrick Carmo <patrick.a.carmo@gmail.com>
2018-08-26 19:33:40 +0100

    Validacao Mongoose

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = 3004;
+const port = 3003;
 
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -8,7 +8,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 server.listen(port, function () {
-    console.log(`BACKEND is running on port ${port}.`);
+    console.log(`BACKEND is running on port ${port}.`)
 });
 
 module.exports = server
